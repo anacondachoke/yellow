85a6ac125107b798aedfb4404b6de4364d718828
architecttoni <metro.anton@yahoo.com>
2020-05-08 16:46:41 +0300

    Start

---
+++ yellow/Source.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+#include <sstream>
+#include <exception>
+#include <iomanip>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
